db44a0721416c7493d5e2a568041d9a735dd12af
Movies <leonardlamparter@gmail.com>
2026-05-21 16:35:06 +0200

    26.1.1-01c

--- obj/Debug/net10.0/BulletboxServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/BulletboxServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulletboxServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a949b49e0eb47d981e07297ac2ebe1dd13af15f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c10e7c790da403787afea4380c6c89aaa447813")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulletboxServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulletboxServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
